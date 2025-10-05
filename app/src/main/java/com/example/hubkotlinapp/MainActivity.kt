21145207f10d3459296959fecfd4ff5485b2bea3
Walteraf <eng.walteraraujo@gmail.com>
2025-10-05 08:15:16 -0300

    fix: corrigindo conexão entre o hub e os aplicativos como novo botões

--- app/src/main/java/com/example/hubkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/hubkotlinapp/MainActivity.kt
@@ -1,31 +1,39 @@
 package com.example.hubkotlinapp
 
-import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import com.example.hubkotlinapp.basquete.BasqueteActivity
 import com.example.hubkotlinapp.calculadora.CalculadoraActivity
 import com.example.hubkotlinapp.Conversor.ConversorActivity
+import android.widget.ImageView
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonBasquete = findViewById<Button>(R.id.buttonBasquete)
-        val buttonCalculadora = findViewById<Button>(R.id.buttonCalculadora)
-        val buttonConversor = findViewById<Button>(R.id.buttonConversor)
+        val cardBasquete = findViewById<ImageView>(R.id.basquete_card)
+        val cardCalculadora = findViewById<ImageView>(R.id.calculadora_card)
+        val cardConversor = findViewById<ImageView>(R.id.conversor_card)
 
-        buttonBasquete.setOnClickListener {
-            startActivity(Intent(this, BasqueteActivity::class.java))
+        cardBasquete.setOnClickListener {
+            // Inicia a atividade de Basquete
+            val intent = Intent(this, BasqueteActivity::class.java) // Adapte o nome da sua Activity
+            startActivity(intent)
         }
 
-        buttonCalculadora.setOnClickListener {
-            startActivity(Intent(this, CalculadoraActivity::class.java))
+        cardCalculadora.setOnClickListener {
+            // Inicia a atividade da Calculadora
+            val intent = Intent(this, CalculadoraActivity::class.java) // Adapte o nome da sua Activity
+            startActivity(intent)
         }
-        buttonConversor.setOnClickListener {
-            startActivity(Intent(this, ConversorActivity::class.java))
+
+        cardConversor.setOnClickListener {
+            // Inicia a atividade do Conversor
+            val intent = Intent(this, ConversorActivity::class.java) // Adapte o nome da sua Activity
+            startActivity(intent)
         }
     }
 }
